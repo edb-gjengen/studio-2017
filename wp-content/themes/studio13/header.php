c9c9c6314f93f76eb5bc2a5a8d30c35709ea0c68
Nikolai Kristiansen <nikolaik@gmail.com>
2013-05-07 09:28:45 +0200

    title

--- wp-content/themes/studio13/header.php
+++ wp-content/themes/studio13/header.php
@@ -8,7 +8,7 @@
         <meta charset="utf-8">
         <!-- Google Chrome Frame for IE -->
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title><?php wp_title(''); ?></title>
+        <title><?php wp_title(' '); ?><?php bloginfo('name'); ?></title>
         <!-- mobile meta (hooray!) -->
         <meta name="HandheldFriendly" content="True">
         <meta name="MobileOptimized" content="320">
