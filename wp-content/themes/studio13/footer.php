32fa2ccd41eeea4e5e8be1cf7a8cb5f9c4c4bb87
Nikolai Kristiansen <nikolaik@gmail.com>
2013-05-07 23:42:13 +0200

    Moved GA from footer to header

--- wp-content/themes/studio13/footer.php
+++ wp-content/themes/studio13/footer.php
@@ -11,19 +11,5 @@
         </footer> <!-- end footer -->
     </div> <!-- end #container -->
     <?php wp_footer(); ?>
-    <script type="text/javascript">
-
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-52914-3']);
-      _gaq.push(['_setDomainName', 'studiofestivalen.no']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-
-    </script>
     </body>
 </html>
--- wp-content/themes/studio13/header.php
+++ wp-content/themes/studio13/header.php
@@ -34,7 +34,21 @@
         <?php wp_head(); ?>
         <!-- end of wordpress head -->
 
-        <!-- drop Google Analytics Here -->
+        <!-- Google Analytics -->
+        <script type="text/javascript">
+
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-52914-3']);
+          _gaq.push(['_setDomainName', 'studiofestivalen.no']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+
+        </script>
         <!-- end analytics -->
         <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
 
