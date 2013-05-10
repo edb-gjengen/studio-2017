de97923a846b1bdc9492fcfb3814493b8adb7d45
Nikolai Kristiansen <nikolaik@gmail.com>
2013-05-10 11:21:07 +0200

    Small fixes

--- wp-content/themes/studio13/header.php
+++ wp-content/themes/studio13/header.php
@@ -8,7 +8,7 @@
         <meta charset="utf-8">
         <!-- Google Chrome Frame for IE -->
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title><?php wp_title(' ', True, 'right'); if(is_home() || is_page()) { bloginfo('name');  } ?></title>
+        <title><?php wp_title(); ?></title>
         <!-- mobile meta (hooray!) -->
         <meta name="HandheldFriendly" content="True">
         <meta name="MobileOptimized" content="320">
